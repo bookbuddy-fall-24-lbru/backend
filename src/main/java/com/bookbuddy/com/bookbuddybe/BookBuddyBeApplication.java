02693f293d295c4c13b66046c6ed32056ba7f301
Lauren Brubaker <ldelisle@creditacceptance.com>
2024-11-12 09:31:24 -0500

    First commit boilerplate

---
+++ src/main/java/com/bookbuddy/com/bookbuddybe/BookBuddyBeApplication.java
@@ -0,0 +1,13 @@
+package com.bookbuddy.com.bookbuddybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBuddyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBuddyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookbuddy/com/bookbuddybe/BookBuddyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbuddy.com.bookbuddybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBuddyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
